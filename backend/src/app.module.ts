f9376cd25a667cd8947bdbf625dbc3d5cad79330
Ihcaer <jakub2002410@wp.pl>
2026-01-09 02:48:31 +0100

    feat<backend>: disable env cache in tests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,15 @@ import { Environment } from '@core/config/server.config';
 import enabledModules from '@core/config/feature-flags.config';
 import { getEnvPath } from '@core/config/get-env-path.util';
 
+const nodeEnv =
+  (process.env.NODE_ENV as Environment) || Environment.Development;
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      ignoreEnvFile: process.env.NODE_ENV === Environment.Production,
+      ignoreEnvFile: nodeEnv === Environment.Production,
       envFilePath: getEnvPath(),
-      cache: true,
+      cache: nodeEnv !== Environment.Test,
     }),
     EventEmitterModule.forRoot({ maxListeners: 3 }),
     ...enabledModules,
