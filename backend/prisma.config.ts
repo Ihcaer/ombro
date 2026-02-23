1040a56de66e4469412af09503c9108a786af959
Ihcaer <97183997+Ihcaer@users.noreply.github.com>
2026-02-23 03:30:53 +0100

    Chore/update dependencies (#21)
    * chore: update dependencies
    
    * refactor: adapt docker-compose files to .env config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,14 +1,25 @@
 import { existsSync } from 'node:fs';
+import { join } from 'node:path';
 import { loadEnvFile } from 'node:process';
 import { defineConfig, env } from 'prisma/config';
 
-const envFileName: string = '.env.local';
+const envSources: string[] = [
+  join(__dirname, '..', '.env'),
+  join(__dirname, '..', '.env.local'),
+  join(__dirname, '..', '.env.test'),
+  join(__dirname, '.env'),
+  join(__dirname, '.env.local'),
+  join(__dirname, '.env.test'),
+  join(__dirname, '..', '.env.example'),
+] as string[];
 
-if (existsSync(envFileName)) {
-  const envFromFile = loadEnvFile(envFileName) ?? {};
-  for (const [key, value] of Object.entries(envFromFile)) {
-    if (typeof value === 'string' && !(key in process.env)) {
-      process.env[key] = value;
+for (const envPath of envSources) {
+  if (existsSync(envPath)) {
+    const envFromFile = loadEnvFile(envPath) ?? {};
+    for (const [key, value] of Object.entries(envFromFile)) {
+      if (typeof value === 'string' && !(key in process.env)) {
+        process.env[key] = value;
+      }
     }
   }
 }
