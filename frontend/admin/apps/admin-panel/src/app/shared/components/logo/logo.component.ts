4e231d33eeae2286ad227cae55bd959cfad5d987
Ihcaer <jakub2002410@wp.pl>
2026-04-25 01:10:19 +0200

    feat(frontend-admin-app): create logo component and add correct favicon

---
+++ frontend/admin/apps/admin-panel/src/app/shared/components/logo/logo.component.ts
@@ -0,0 +1,26 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+
+export type logoTypes = 'wordmark' | 'lettermark';
+
+@Component({
+  selector: 'app-logo',
+  imports: [NgOptimizedImage],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LogoComponent {
+  private static readonly LOGO_PATH = 'assets/icons/brand/';
+  private static readonly LOGO_MAP: Record<logoTypes, string> = {
+    wordmark: 'wordmark.svg',
+    lettermark: 'lettermark.svg',
+  };
+
+  /** @see {@link LogoComponent} */
+  readonly type = input.required<logoTypes>();
+
+  protected readonly imageSrc = computed(
+    () => LogoComponent.LOGO_PATH + LogoComponent.LOGO_MAP[this.type()],
+  );
+}
