5a64ac8b81a618779470fd14114517937b0f8f56
Ihcaer <jakub2002410@wp.pl>
2026-06-16 15:49:17 +0200

    chore(infrastructure): change infrastructure configuration
    changes:
    - change nginx configuration
    - replace minIO with seaweedfs
    - change nginx configuration in docker compose
    - add mailpit to local docker compose
    - reorganize .env.example file
    - update README file
    - add more words to spell checker settings

--- frontend/admin/apps/admin-panel/src/environments/environment.example.ts
+++ frontend/admin/apps/admin-panel/src/environments/environment.example.ts
@@ -2,7 +2,7 @@ import { EnvConfig } from '../app/core/config/interfaces/env-config.interface';
 
 export const environment: EnvConfig = {
   production: false,
-  apiDomain: 'localhost:3000',
+  apiDomain: '',
   apiSlug: '/api/v1',
   coreFeatures: {},
   addonFeatures: {},
