1f963ad08f9cbaf0d801a9865bc9778b01f97853
Ihcaer <jakub2002410@wp.pl>
2026-06-29 17:28:52 +0200

    fix(frontend-admin-app): add '/' to requests's paths and authInterceptor in appConfig

--- frontend/admin/apps/admin-panel/src/app/app.config.ts
+++ frontend/admin/apps/admin-panel/src/app/app.config.ts
@@ -9,12 +9,15 @@ import { providePrimeNG } from 'primeng/config';
 import { DefaultPreset } from '@ombro/themes';
 import { provideMaterialSymbols } from '@ombro/shared/ui-icons';
 import { PRIME_NG_PL } from './core/config/i18n/primeng-pl';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({
       theme: {
         preset: DefaultPreset,
