403f65be5fdff2075da135add1337917bb5ea859
Ihcaer <jakub2002410@wp.pl>
2026-08-21 22:50:22 +0200

    fix(frontend-admin-app): fix dto paths

--- frontend/admin/apps/admin-panel/src/app/core/auth/types/admin-data.types.ts
+++ frontend/admin/apps/admin-panel/src/app/core/auth/types/admin-data.types.ts
@@ -1,25 +1,5 @@
-/* export type AdminPrivilege =
-  | 'ADMINS_MANAGE'
-  | 'FILE_MANAGE'
-  | 'BLOG_MANAGE'
-  | 'SUPER_ADMIN'
-  | 'OWNER'; */
-
 import { AdminDtoPrivilegesItem } from '@ombro/shared/data-access/api-client';
 
 export type AdminPrivileges = Set<AdminDtoPrivilegesItem>;
-
-/* export type AdminVerification = 'VERIFIED' | 'WAITING' | 'NON_VERIFIED';
-
-export type Admin = {
-  id: number;
-  displayName: string;
-  handleName: string;
-  avatarUrl: string | null;
-  privileges: AdminPrivileges;
-  verification: AdminVerification;
-  isActivated: boolean;
-  email?: string;
-}; */
 
 export type AdminPassword = string;
