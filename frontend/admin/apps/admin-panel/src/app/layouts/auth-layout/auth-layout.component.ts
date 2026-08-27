945a773e9b6043d573e272d4d97362c8fabff0e8
Ihcaer <jakub2002410@wp.pl>
2026-08-27 14:24:33 +0200

    fix(frontend-internalization): fix translation scopes providing

--- frontend/admin/apps/admin-panel/src/app/layouts/auth-layout/auth-layout.component.ts
+++ frontend/admin/apps/admin-panel/src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideTranslocoScope } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-auth-layout',
   imports: [RouterOutlet],
+  providers: [provideTranslocoScope('auth')],
   template: `<div class="layout"><router-outlet /></div> `,
   styles: `
     @use '../common.scss';
