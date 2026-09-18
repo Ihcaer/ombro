601d011b99faadaaf16df22e58c23f885f9fd8ca
Ihcaer <97183997+Ihcaer@users.noreply.github.com>
2026-09-18 14:43:54 +0200

    fix(frontend-admin-app): fix display of avatar, rewrite sidebar active item logic, fix redirect after logout (#61)

--- frontend/admin/apps/admin-panel/src/app/layouts/panel-layout/components/sidebar/sidebar.component.ts
+++ frontend/admin/apps/admin-panel/src/app/layouts/panel-layout/components/sidebar/sidebar.component.ts
@@ -2,16 +2,21 @@ import { ChangeDetectionStrategy, Component, computed, inject, model, signal } f
 import { IconComponent } from '@ombro/shared/ui/ui-icons';
 import { LogoComponent } from '@ombro/admin-panel/app/shared/components/logo/logo.component';
 import { SIDE_MENU_ITEMS, SideMenuItem } from './menu-items';
-import { NavigationEnd, Router, RouterLink } from '@angular/router';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { filter } from 'rxjs';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthStore } from '@ombro/admin-panel/app/core/auth/store';
 import { NgTemplateOutlet } from '@angular/common';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-panel-layout-sidebar',
-  imports: [IconComponent, LogoComponent, NgTemplateOutlet, RouterLink, TranslocoDirective],
+  imports: [
+    IconComponent,
+    LogoComponent,
+    NgTemplateOutlet,
+    RouterLink,
+    TranslocoDirective,
+    RouterLinkActive,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,37 +24,17 @@ import { TranslocoDirective } from '@jsverse/transloco';
 export class SidebarComponent {
   isCollapsed = model.required<boolean>();
 
-  private readonly router = inject(Router);
   private readonly authStore = inject(AuthStore);
 
   protected navAriaLabel = signal<string>('Nawigacja główna');
-
-  private readonly navigationEndEvent = toSignal(
-    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)),
-    { initialValue: new NavigationEnd(0, this.router.url, this.router.url) },
-  );
-
-  private allMenuItems: SideMenuItem[] = SIDE_MENU_ITEMS;
+  private allMenuItems = signal<SideMenuItem[]>(SIDE_MENU_ITEMS);
 
   protected readonly menuItems = computed<SideMenuItem[]>(() => {
-    const url = this.navigationEndEvent().urlAfterRedirects;
-    const urlTree = this.router.parseUrl(url);
+    const allItems = this.allMenuItems();
 
-    const urlCategoryIndex: number = 1;
-    const categorySegment: string = urlTree.root.children['primary']?.segments.map(
-      (segment) => segment.path,
-    )[urlCategoryIndex];
-
-    return this.allMenuItems
-      .filter((item) => this.authStore.hasAppropriatePrivileges(item.neededPrivileges))
-      .map((item) => {
-        const url = item.link;
-        const itemCategory: string = url.split('/').filter(Boolean)[urlCategoryIndex];
-        return {
-          ...item,
-          isActive: itemCategory === categorySegment && itemCategory !== undefined,
-        };
-      });
+    return allItems.filter((item) =>
+      this.authStore.hasAppropriatePrivileges(item.neededPrivileges),
+    );
   });
 
   changeMenuStatus(): void {
