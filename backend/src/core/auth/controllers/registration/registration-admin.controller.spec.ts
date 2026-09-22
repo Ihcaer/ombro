aaf90bc92583a8f001a1e273a2815120688f8c0c
Ihcaer <jakub2002410@wp.pl>
2026-09-22 18:01:14 +0200

    fix(backend-tests): fix backend unit tests by adding guard mock

--- backend/src/core/auth/controllers/registration/registration-admin.controller.spec.ts
+++ backend/src/core/auth/controllers/registration/registration-admin.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegistrationAdminController } from './registration-admin.controller';
 import { AdminRegistrationService } from '@core/auth/services/admin-registration/admin-registration.service';
+import { AccessTokenGuard } from '@core/auth/guards/access-token.guard';
+import { mockGuard } from '@shared/testing/mock-guard';
 
 describe('RegistrationAdminController', () => {
   let controller: RegistrationAdminController;
@@ -17,7 +19,10 @@ describe('RegistrationAdminController', () => {
           },
         },
       ],
-    }).compile();
+    })
+      .overrideGuard(AccessTokenGuard)
+      .useValue(mockGuard())
+      .compile();
 
     controller = module.get<RegistrationAdminController>(RegistrationAdminController);
     // adminRegistrationService = module.get(AdminRegistrationService);
